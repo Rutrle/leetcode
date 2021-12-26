3e7acfb3a7394925d2febf14d3a5935260d1c116
Jakub Rutrle <rutrle@seznam.cz>
2021-12-26 02:05:40 +0100

    house robber problem

---
+++ 198_house_robber_2.py
@@ -0,0 +1,12 @@
+def rob(nums: list) -> int:
+    rob1, rob2 = 0, 0
+    for n in nums:
+        temp = max(n+rob1, rob2)
+        rob1 = rob2
+        rob2 = temp
+
+    return rob2
+
+
+print(rob([2, 7, 9, 3, 1]))
+# https: // www.youtube.com/watch?v = 73r3KWiEvyk
